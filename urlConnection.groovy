a190a7bdb85c1f030c5d0d872a5470568aa035a2
Bharat Reddy Sama <bsama@egencia.com>
2020-01-13 19:40:59 -0800

    http connection using groovy

--- urlConnection.groovy
+++ urlConnection.groovy
@@ -1,3 +1,10 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
+
 String url = "http://here"
 
 def getData(String url,
