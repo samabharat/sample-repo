ef3d6b4a85ac7380a315ec51e8c1e4f23c6e721d
Bharat Reddy Sama <bsama@egencia.com>
2020-01-13 19:38:00 -0800

    http connection using groovy

--- urlConnection.groovy
+++ urlConnection.groovy
@@ -1,5 +1,4 @@
-import groovy.json.JsonSlurper
-String url = "https://api.github.com/user"
+String url = "http://here"
 
 def getData(String url,
                    String userName,
@@ -7,16 +6,18 @@ def getData(String url,
     def authString = ("${userName}:${password}").getBytes().encodeBase64().toString()
     def connection = (url).toURL().openConnection()
     connection.addRequestProperty('Authorization', 'Basic ' + authString)
-    connection.addRequestProperty('Content-Type', 'application/json')
+//    connection.addRequestProperty('Content-Type', 'application/json')
     connection.setRequestMethod('GET')
     connection.setReadTimeout(30000)
     try {
         connection.connect()
-        def line = new BufferedReader(new InputStreamReader(connection.getInputStream())).readLine()
-        def jsonSlurper = new JsonSlurper()
-        def response=jsonSlurper.parseText(line)
-        return response
-
+        def reader = new BufferedReader(new InputStreamReader(connection.getInputStream()))
+          String line = null;
+          while ((line = reader.readLine()) != null) {
+              System.out.println(line);
+          }
+          inputStream.close();
+           
     } finally {
         connection.disconnect();
     }
@@ -24,5 +25,4 @@ def getData(String url,
 
 def myData = getData(url,"username","password")
 println(myData) 
-// To access individual response object just use myData.login etc...
-//println(myData.logi
+
